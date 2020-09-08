63f3a511e7c4b1e2cb5e952ae84c7a881ab1084d
kzmaster1216 <13260634356@163.com>
2020-09-08 16:48:53 +0800

    初始化

---
+++ src/Jssdk.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/9 0009
+ * Time: 下午 16:25
+ */
+
+class Jssdk
+{
+    private $appId;
+    private $appSecret;
+
+    public function __construct($appId, $appSecret)
+    {
+        $this->appId = $appId;
+        $this->appSecret = $appSecret;
+    }
+
+    public function getSignPackage($url)
+    {
+        $jsapiTicket = $this->getJsApiTicket();
+        //$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $timestamp = time();
+        $nonceStr = $this->createNonceStr();
+
+        // 这里参数的顺序要按照 key 值 ASCII 码升序排序
+        $string = "jsapi_ticket=".$jsapiTicket."&noncestr=".$nonceStr."&timestamp=".$timestamp."&url=".$url;
+        $signature = sha1($string);
+        $signPackage = array(
+            "appId" => $this->appId,
+            "nonceStr" => $nonceStr,
+            "timestamp" => $timestamp,
+            "url" => $url,
+            "signature" => $signature,
+            "rawString" => $string
+        );
+        return $signPackage;
+    }
+
+    private function createNonceStr($length = 16)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
+    }
+
+    private function getJsApiTicket()
+    {
+        // jsapi_ticket 应该全局存储与更新，以下代码以写入到文件中做示例
+        $data = \Cache::has('jsapi_ticket');
+        if (empty($data)) {
+            $accessToken = $this->getAccessToken();
+            $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+            $res = json_decode($this->httpGet($url));
+            $ticket = $res->ticket;
+            if ($ticket) {
+                \Cache::add('jsapi_ticket',$ticket, 7200);
+            }
+        } else {
+            $ticket = \Cache::get('jsapi_ticket');
+        }
+
+        return $ticket;
+    }
+
+    private function getAccessToken()
+    {
+        // access_token 应该全局存储与更新，以下代码以写入到文件中做示例
+        $data = \Cache::has('access_token');
+        if (empty($data)){
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appId&secret=$this->appSecret";
+            $res = json_decode($this->httpGet($url));
+            $access_token = $res->access_token;
+            if ($access_token) {
+                \Cache::add('access_token',$access_token, 7200);
+            }
+        } else {
+            $access_token = \Cache::get('access_token');;
+        }
+        return $access_token;
+    }
+
+    private function httpGet($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $res = curl_exec($curl);
+        curl_close($curl);
+        return $res;
+    }
+
+}
